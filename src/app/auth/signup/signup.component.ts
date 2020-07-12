9ae8dfdc0463c31adf93202233d010394ec55345
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 15:49:46 -0400

    created signup fields with basic validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+
+  authForm = new FormGroup({
+
+    userName: new FormControl('', 
+    [
+        Validators.required, 
+        Validators.minLength(3), 
+        Validators.maxLength(20), 
+        Validators.pattern(/^([a-z]|[0-9])+$/)
+    ]),
+    password: new FormControl('',
+    [
+        Validators.required, 
+        Validators.minLength(6), 
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/)
+    ] 
+    ),
+    passwordConfirmation: new FormControl('', 
+    [
+      Validators.required, 
+      Validators.minLength(6), 
+      Validators.maxLength(20), 
+      Validators.pattern(/^[a-z0-9]+$/)
+    ])
+
+  });
 
   constructor() { }
 
