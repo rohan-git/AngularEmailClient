a4ae4ebb6fb4bd23efa5b544c6c30c50f078a582
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 16:17:39 -0400

    injecting and using custom validator

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { MatchPassword } from '../validators/match-password';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+
+  constructor(private matchPassword: MatchPassword) { }
 
   authForm = new FormGroup({
 
@@ -21,21 +25,20 @@ export class SignupComponent implements OnInit {
     [
         Validators.required, 
         Validators.minLength(6), 
-        Validators.maxLength(20),
-        Validators.pattern(/^[a-z0-9]+$/)
-    ] 
-    ),
+        Validators.maxLength(20)
+    ]),
     passwordConfirmation: new FormControl('', 
     [
       Validators.required, 
       Validators.minLength(6), 
-      Validators.maxLength(20), 
-      Validators.pattern(/^[a-z0-9]+$/)
+      Validators.maxLength(20)
     ])
 
-  });
+  }, {
+    validators: [this.matchPassword.validate]
+  }
+  );
 
-  constructor() { }
 
   ngOnInit(): void {
   }
