26b22eb6181f99ee6bdf31db9577c85f4ebd45fe
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 19:39:21 -0400

    adding SharedModule to auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignoutComponent } from './signout/signout.component';
@@ -12,7 +14,7 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [SigninComponent, SignoutComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule, ReactiveFormsModule
+    AuthRoutingModule, ReactiveFormsModule, SharedModule
   ]
 })
 export class AuthModule { }
