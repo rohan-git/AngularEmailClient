0b44675d9a69e488be37443b652254001b6ae871
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 19:15:55 -0400

    Async validator attached to Service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  usernameAvailable(username: string){
+
+    return this.http.post<{available: boolean}>('https://localhost:8081/auth/username', {
+            username: username
+        });
+  }
 }
--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,37 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { FormControl, AsyncValidator } from '@angular/forms';
 
+import { of } from 'rxjs';
 import {  map, catchError } from 'rxjs/operators';
-import { observable, Observable, of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator {
 
-    constructor(private http: HttpClient) { }
+    constructor(private authService: AuthService) { }
 
     validate = (formControl: FormControl) => {
 
         const { value } = formControl;
         console.log(value);
 
-        return this.http.post<any>('https://localhost:8081/auth/username', {
-            username: value
-        })
-        .pipe(
-            map(value => {
-                    if(value.available) {
-                        return null;
-                    }
-                }
-            ), catchError((error) => {
+        return this.authService.usernameAvailable(value)
+                .pipe(
+                    map(value => {
+                            if(value.available) {
+                                return null;
+                            }
+                        }
+                    ), catchError((error) => {
 
-                console.log(error);
-                return of({availableUsername: false});
-            })
-        );
-
-
-
+                        console.log(error);
+                        return of({availableUsername: false});
+                    })
+                );
     }
 }
