dcfd3b2988b0344eeaff93a7d2a61c10074808f1
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 11:29:55 -0400

    added routable components to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+
 
 const routes: Routes = [];
 
