e8984daca5207dbec03427044493be2f9f55e130
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 16:17:18 -0400

    custom validator is injectable now

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { Validator, FormGroup } from '@angular/forms';
  
+@Injectable({providedIn: 'root'})
 export class MatchPassword implements Validator {
 
     validate(formGroup: FormGroup){
