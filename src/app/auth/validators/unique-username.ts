e4bf79e9ccbea4ce61c357d93014b5beca09cb53
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 17:33:54 -0400

    created async unique username validator

---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, AsyncValidator } from '@angular/forms';
+
+@Injectable({providedIn: 'root'})
+export class UniqueUsername implements AsyncValidator {
+
+    constructor(private http: HttpClient) { }
+
+    validate = (formControl: FormControl) => {
+
+        const { value } = formControl;
+        console.log(value);
+
+        return null;
+    }
+}
