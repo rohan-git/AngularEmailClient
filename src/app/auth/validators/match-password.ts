4586a664464aaeb0f8e31de250165113dcc0b4f3
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-12 16:06:31 -0400

    added custom validator ot compare 2 diff fields in form

---
+++ src/app/auth/validators/match-password.ts
@@ -0,0 +1,16 @@
+import { Validator, FormGroup } from '@angular/forms';
+ 
+export class MatchPassword implements Validator {
+
+    validate(formGroup: FormGroup){
+
+        const { password, passwordConfirmation } = formGroup.value;
+
+        if(password === passwordConfirmation) {
+            return null;
+        }
+        else {
+            return { passwordsDontMatch: true };
+        }
+    }
+}
