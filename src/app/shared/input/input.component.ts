d3f0062508a4816d31fee4d3621406230aa5d5fe
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-13 17:09:01 -0400

    Updated shared module with variety of error messages for diff. situations

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -7,7 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  @Input() label = '';
+  @Input() control = new FormControl();
+  @Input() inputType = '';
+
   constructor() { }
+
+  showErrors(){
+
+    //console.log(this.control);
+    
+    const {dirty, touched, errors } = this.control;
+
+    return dirty && touched && errors;
+  }
+
+  getUniqueUsernameError(){
+
+    if(this.control.errors.uniqueUsername){
+      return "Username not available, please try another username";
+    }
+
+    if(this.control.errors.asyncError) {
+      return "Error occured trying to test for unique username";
+    }
+
+  }
 
   ngOnInit(): void {
   }
