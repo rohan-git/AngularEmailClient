198055f393b95daf44f3eee42d3e32dba0d33fb4
rohan <1411081+rohan-git@users.noreply.github.com>
2020-07-20 16:32:10 -0400

    Getting Signed in observable's result in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
   title = 'AngularEmailClient';
+  signedIn = false;
+
+  constructor(private authService: AuthService){}
+
+  ngOninit() {
+    this.authService.signedIn$.subscribe(signedIn=> {
+      this.signedIn = signedIn
+    }); 
+  }
+
+  
 }
